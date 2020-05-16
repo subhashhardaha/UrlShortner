ff327e0bbb1083e7112b14b6298a41a361a3794c
Subhash Hardaha <subhashhardaha@gmail.com>
2020-05-16 22:22:37 +0530

    Added active and manager to return only active URLS

--- tracker/models.py
+++ tracker/models.py
@@ -6,9 +6,16 @@ from .utils import create_shortcode
 
 # Create your models here.
 
+class AffiliateURLManager(models.Manager):
+    def all(self,*args,**kwarg):
+        qs_main=super(AffiliateURL,self).all(*args,**kwarg)
+        qs=qs_main.filter(active=True)
+        return qs
+
 class AffiliateURL(models.Model):
     url = models.CharField(max_length=220, )
     shortcode = models.CharField(max_length=50,unique=True,blank=True)
+    active=models.BooleanField(default=True)
     updated=models.DateTimeField(auto_now=True)
     timestamp=models.DateTimeField(auto_now_add=True)
 
