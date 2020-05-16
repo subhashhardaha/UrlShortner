b89fba5a20357c56425ee5608b0abb6efcd98ccd
Subhash Hardaha <subhashhardaha@gmail.com>
2020-05-16 21:41:49 +0530

    Added model AffliateURl

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
+import random
+import string
+
 from django.db import models
 
+
 # Create your models here.
+
+class AffiliateURL(models.Model):
+    url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=50)
+    updated=models.DateTimeField(auto_now=True)
+    timestamp=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.url)
