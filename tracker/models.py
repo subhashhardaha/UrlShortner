55179a6c0de673b249a84f32eaea3b310fc04c00
Subhash Hardaha <subhashhardaha@gmail.com>
2020-05-16 21:57:04 +0530

    Imported utils and added logic for blank shortcode

--- tracker/models.py
+++ tracker/models.py
@@ -2,21 +2,20 @@ import random
 import string
 
 from django.db import models
-
-def code_generator(size=6,char=string.ascii_letters+string.digits):
-    return ''.join(random.choice(char) for _ in range(size))
+from .utils import code_generator
 
 # Create your models here.
 
 class AffiliateURL(models.Model):
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=50)
+    shortcode = models.CharField(max_length=50,unique=True,blank=True)
     updated=models.DateTimeField(auto_now=True)
     timestamp=models.DateTimeField(auto_now_add=True)
 
 
     def save(self,*args,**kwargs):
-        self.shortcode=code_generator()
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode=code_generator()
         super(AffiliateURL,self).save(*args,**kwargs)
 
     def __str__(self):
