e33d91c6d61eba13d6b1707310f23e28a07cca00
Subhash Hardaha <subhashhardaha@gmail.com>
2020-05-16 21:44:10 +0530

    Added code to generate shortcode

--- tracker/models.py
+++ tracker/models.py
@@ -3,6 +3,8 @@ import string
 
 from django.db import models
 
+def code_generator(size=6,char=string.ascii_letters+string.digits):
+    return ''.join(random.choice(char) for _ in range(size))
 
 # Create your models here.
 
@@ -11,6 +13,11 @@ class AffiliateURL(models.Model):
     shortcode = models.CharField(max_length=50)
     updated=models.DateTimeField(auto_now=True)
     timestamp=models.DateTimeField(auto_now_add=True)
+
+
+    def save(self,*args,**kwargs):
+        self.shortcode=code_generator()
+        super(AffiliateURL,self).save(*args,**kwargs)
 
     def __str__(self):
         return str(self.url)
